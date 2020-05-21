90ca001d102858d499a3f27400de2ff88c4b8964
Robert <chelu.g.robert@gmail.com>
2020-05-21 20:46:49 +0300

    Removed authentication, added git conf shortcut

--- common/build-all.sh
+++ common/build-all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-gcloud auth login
+#gcloud auth login
 
 source project_settings.sh
-
+echo $PROJECT_ID
 gcloud config set project $PROJECT_ID
 gcloud config set compute/region $REGION
 gcloud config set compute/zone $ZONE
